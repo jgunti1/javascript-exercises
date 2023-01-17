60e2b952d723c436280c94c1a27d99fb15a0480b
Jordan Gunti <jgunti1@gmail.com>
2023-01-17 11:56:57 -0600

    Fix problem 4, need to stop cheating

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numList,...args) {
+    /*for (var i=0;i<numList.length; i++) {
+        if (numList[i]==removeNum) {
+            numList.splice(i,1);
+        }
+    }
+    return numList;
+    */
+    return numList.filter(x => !args.includes(x))
 };
 
 // Do not edit below this line
