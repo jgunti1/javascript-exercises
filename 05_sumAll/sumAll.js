449ed33abf2c4f57ebd05588935f5568424a7924
Jordan Gunti <jgunti1@gmail.com>
2023-01-24 11:03:55 -0600

    Fix problem 5, on my own

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    var count = 0;
+    if (first > second) {
+        temp = second;
+        second = first;
+        first = temp;
+    }
+    else if ( first <0 || second <0) {
+        return 'ERROR';
+    }
+    else if (typeof first != 'number' || typeof second != 'number') {
+        return 'ERROR';
+    }
+    
+    for (let i=first;i<=second;i++)
+        {
+            count += i;
+        }
+    return count;
 };
 
 // Do not edit below this line
