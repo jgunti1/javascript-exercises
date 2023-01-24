4bbe222d7d4a3e1a035bf9372c48e6e25ccb5d0d
Jordan Gunti <jgunti1@gmail.com>
2023-01-24 11:44:29 -0600

    Fix problem 7, had trouble keeping track of types

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var celsius = (temp - 32) * (5/9);
+  if (Number.isInteger(celsius)){
+    return celsius;
+  }
+  else {
+    return parseFloat(celsius.toFixed(1));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var fahrenheit = (temp * (9/5) + 32);
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  }
+  else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
+  
 };
 
 // Do not edit below this line
