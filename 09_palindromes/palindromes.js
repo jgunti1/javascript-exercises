b45962ce8bb1c2bc3f5ea7c9335f02772ffad6ce
Jordan Gunti <jgunti1@gmail.com>
2023-02-23 16:16:53 -0600

    Need to stop cheating

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = palindrome.toLowerCase();
+    palindrome = palindrome.replace(/[^a-z]/g,"");
+    newPalindrome = '';
+    for (let i = palindrome.length - 1; i>=0;i-- ){
+        newPalindrome += palindrome[i];
+    }
+    return palindrome == newPalindrome ? true : false ;
 };
 
 // Do not edit below this line
