f1e2f1abc8f1145161dbf1c0cc66bbb44eaed7a3
Jordan Gunti <jgunti1@gmail.com>
2023-02-25 15:07:53 -0600

    Fib is hard

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a<0) {
+        return 'OOPS';
+    }
+    let seq = parseInt(a);
+    let first =0;
+    let second =1;
+    for (let i = 1; i<seq; i++) {
+        let temp = first + second;
+        first = second
+        second = temp
+    }
+    return second;
 };
 
 // Do not edit below this line
